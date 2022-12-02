8f55456ac8cd2d5755f2117f56ee5df537f64dd3
Chuimao <2438232742@qq.com>
2022-12-02 16:37:22 +0800

    commit-1

---
+++ git_hello/src/main/java/com/wu/GitTest.java
@@ -0,0 +1,12 @@
+package com.wu;
+
+/**
+ * @ Author     ：ChuiMao Wu
+ * @ create     : 2022-12-02 16:36
+ * @ Description：
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
