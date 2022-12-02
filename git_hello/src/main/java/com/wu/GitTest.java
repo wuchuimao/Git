9841dd673b3e132947dc5f2b1e18a34ca7b0e37f
Chuimao <2438232742@qq.com>
2022-12-02 17:58:30 +0800

    commit-hot-fix

--- git_hello/src/main/java/com/wu/GitTest.java
+++ git_hello/src/main/java/com/wu/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!-2");
         System.out.println("hello git!-3");
+        System.out.println("test hot-fix");
 
     }
 }
